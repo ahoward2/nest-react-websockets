04e866b6039397b08e6df38d0c78d3ab18b8d5b4
Austin Howard <ahoward@austinhoward.tech>
2022-12-09 09:39:37 -0500

    remove unused metadata arg in chat pipe

--- src/server/chat/pipes/chat.pipe.ts
+++ src/server/chat/pipes/chat.pipe.ts
@@ -1,16 +1,11 @@
-import {
-  PipeTransform,
-  Injectable,
-  ArgumentMetadata,
-  BadRequestException,
-} from '@nestjs/common';
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 import { Schema } from 'zod';
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: Schema) {}
 
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: any) {
     const { error } = this.schema.parse(value);
     if (error) {
       throw new BadRequestException('Validation failed');

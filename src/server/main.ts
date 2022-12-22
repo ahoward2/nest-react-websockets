8a4be1d6ba6a00e18c6d44c47eb1c1717e5ebc91
Austin Howard <ahoward@austinhoward.tech>
2022-12-22 10:34:28 -0500

    remove helmet for the time being

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,18 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        'script-src': ["'self'", "'unsafe-eval'"],
-        'style-src': ["'unsafe-inline'"],
-      },
-    }),
-  );
+
   await app.listen(3000);
 
   // Gracefully shutdown the server.
